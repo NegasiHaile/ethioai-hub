d7fcfa61edcca9eb74af68756b7fdd8aa912ab32
NegasiHiale <NegasiHaile.Abadi@gmail.com>
2024-11-19 19:34:08 +0300

    Added: Content filter options

---
+++ src/utils/constants.ts
@@ -0,0 +1,14 @@
+export const filterOptions = {
+  content: ["Dataset", "Model", "Paper"],
+  fields: ["NLP", "Computer Vision", "Speech Recognition", "Generative AI"],
+  domains: [
+    "Helath",
+    "Language",
+    "Politics",
+    "Education",
+    "Agriculture",
+    "Bussiness",
+    "Art",
+    "Constraction",
+  ],
+};
