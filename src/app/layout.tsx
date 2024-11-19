642e0422bc34eb77fb8dd6222bab6907a5dfd8f1
NegasiHiale <NegasiHaile.Abadi@gmail.com>
2024-11-19 22:48:46 +0300

    Updated: Main layout content for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,12 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Ethio Models Hub",
-  description: "Ethiopian related dataset and ML modles hub",
+  title: "Ethio Models Hub | Ethiopian NLP, AI Models, and Datasets",
+  description:
+    "Explore Ethiopian-related NLP models, AI models, Amharic, Tigrinya, and Oromo datasets. Discover the best research and models in Ethiopia for Machine Learning and AI applications.",
+  keywords:
+    "Ethiopian models, Ethio NLP, AI models in Ethiopia, best researches in Ethiopia, dataset in Ethiopia, Amharic dataset, Tigrinya dataset, Oromo dataset, Ethiopian AI models",
+  robots: "index, follow", // Ensure the page can be crawled and indexed by search engines
 };
 
 export default function RootLayout({
