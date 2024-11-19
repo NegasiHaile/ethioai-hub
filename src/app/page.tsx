11ebce6bb4e241e56de1526d53e6b0a4ff1c64c1
NegasiHiale <NegasiHaile.Abadi@gmail.com>
2024-11-19 23:36:57 +0300

    Updated: Container width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Projects from "@/components/projects";
 
 export default function Home() {
   return (
-    <div className="w-full sm:w-[90%] md:w-[85] lg:w-[60%] p-5 space-y-5 font-[family-name:var(--font-geist-sans)]">
+    <div className="w-full sm:w-[90%] md:w-[85] lg:w-[80%] xl:w-[65%] p-5 space-y-5 font-[family-name:var(--font-geist-sans)]">
       <div className="w-full pb-2 border-b text-start">
         <p className="w-full font-bold text-3xl">
           Ethiopian Datasets and ML Models-HUB
