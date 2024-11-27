85883773a1cc6bbaa174de408c5c7a8228a2475c
NegasiHiale <NegasiHaile.Abadi@gmail.com>
2024-11-27 16:54:42 +0300

    Updated: Header descriptions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ export default function Home() {
     <div className="w-full sm:w-[90%] md:w-[85] lg:w-[80%] xl:w-[65%] p-5 space-y-5 font-[family-name:var(--font-geist-sans)]">
       <div className="w-full pb-2 border-b text-start">
         <p className="w-full font-bold text-3xl">
-          Ethiopian Datasets and ML Models-HUB
+          AI related Papres, Datasets and ML Models in Ethiopia
         </p>
         <p className="mt-6 opacity-70">
-          Explore Ethiopian-related NLP models, AI models, Amharic, Tigrinya,
-          and Oromo datasets. Discover the best research and models in Ethiopia
-          for Machine Learning and AI applications.
+          Explore Ethiopian-related ML models, Amharic, Tigrinya, and Oromo
+          datasets. Discover the best research and models in Ethiopia for
+          Machine Learning and AI applications.
         </p>
       </div>
 
